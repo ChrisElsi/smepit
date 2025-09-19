a6687fae07cf162b26578a6786becebf21634977
ChrisElsi <christian.elsinger@gmail.com>
2025-09-19 06:08:55 +0200

    Fix Vercel deployment - add configuration files

---
+++ next.config.js
@@ -0,0 +1,11 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: '.next',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+};
+
+module.exports = nextConfig;
